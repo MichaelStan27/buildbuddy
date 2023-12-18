fd299f2e743dcac9d850549eaafbc61c67b05c5d
Exaltation <michstan96@gmail.com>
2023-12-18 21:04:05 +0700

    initial commit

---
+++ src/test/java/com/buildbuddy/BuildbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
