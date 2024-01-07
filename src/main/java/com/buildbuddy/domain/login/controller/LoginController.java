e009883153cc5ffd96ae57eb8088a68c4b83781f
Michael Stanley <michstan96@gmail.com>
2024-01-07 08:38:16 +0700

    rename controller and package home to login

--- src/main/java/com/buildbuddy/domain/login/controller/LoginController.java
+++ src/main/java/com/buildbuddy/domain/login/controller/LoginController.java
@@ -1,11 +1,11 @@
-package com.buildbuddy.domain.home.controller;
+package com.buildbuddy.domain.login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
     @GetMapping("/login")
     public String getLogin(Model model){
         return "login";
