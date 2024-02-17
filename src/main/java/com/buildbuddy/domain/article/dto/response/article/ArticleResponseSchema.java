bb3990ae6e9ee4595e5455dbe7419bfec3bd91b2
Fernando Clemente <fernandoclemente73@gmail.com>
2024-02-17 10:57:22 +0700

    update article get and delete

--- src/main/java/com/buildbuddy/domain/article/dto/response/article/ArticleResponseSchema.java
+++ src/main/java/com/buildbuddy/domain/article/dto/response/article/ArticleResponseSchema.java
@@ -1,4 +1,31 @@
 package com.buildbuddy.domain.article.dto.response.article;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleResponseSchema {
+
+    @JsonProperty(value = "articleList")
+    private List<ArticleResponseDto> articleList;
+
+    @JsonProperty(value = "pageNo")
+    private Integer pageNo;
+
+    @JsonProperty(value = "pageSize")
+    private Integer pageSize;
+
+    @JsonProperty(value = "totalPages")
+    private long totalPages;
+
+    @JsonProperty(value = "totalData")
+    private long totalData;
 }
--- src/main/java/com/buildbuddy/domain/article/service/ArticleService.java
+++ src/main/java/com/buildbuddy/domain/article/service/ArticleService.java
@@ -1,20 +1,29 @@
 package com.buildbuddy.domain.article.service;
 
 import com.buildbuddy.audit.AuditorAwareImpl;
+import com.buildbuddy.domain.article.dto.param.ArticleRequestParam;
 import com.buildbuddy.domain.article.dto.request.ArticleRequestDto;
 import com.buildbuddy.domain.article.dto.response.article.ArticleResponseDto;
+import com.buildbuddy.domain.article.dto.response.article.ArticleResponseSchema;
 import com.buildbuddy.domain.article.entity.ArticleEntity;
 import com.buildbuddy.domain.article.repository.ArticleRepository;
 import com.buildbuddy.domain.user.entity.UserEntity;
 import com.buildbuddy.exception.BadRequestException;
 import com.buildbuddy.jsonresponse.DataResponse;
+import com.buildbuddy.util.PaginationCreator;
+import com.buildbuddy.util.spesification.ParamFilter;
+import com.buildbuddy.util.spesification.SpecificationCreator;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -26,6 +35,62 @@ public class ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @Autowired
+    private SpecificationCreator<ArticleEntity> specificationCreator;
+    
+    @Autowired
+    private PaginationCreator paginationCreator;
+
+    @Transactional
+    public DataResponse<ArticleResponseSchema> get(ArticleRequestParam requestParam) {
+
+        boolean isPaginated = requestParam.isPagination();
+        Integer pageNo = requestParam.getPageNo();
+        Integer pageSize = requestParam.getPageSize();
+        String sortBy = requestParam.getSortBy();
+        String sortDirection = requestParam.getSortDirection();
+
+        Sort sort = paginationCreator.createSort(sortDirection, sortBy);
+
+        Pageable pageable = paginationCreator.createPageable(isPaginated, sort, pageNo, pageSize);
+
+        Page<ArticleEntity> dataPage = getArticleFromDB(requestParam, pageable);
+        List<ArticleEntity> articleList = dataPage.getContent();
+
+        List<ArticleResponseDto> articleResponseDtos = articleList.stream()
+                .map(ArticleResponseDto::convertToDto)
+                .toList();
+
+        ArticleResponseSchema data = ArticleResponseSchema.builder()
+                .articleList(articleResponseDtos)
+                .pageNo(pageNo)
+                .pageSize(pageSize)
+                .totalPages(dataPage.getTotalPages())
+                .totalData(dataPage.getTotalElements())
+                .build();
+
+        return DataResponse.<ArticleResponseSchema>builder()
+                .timestamp(LocalDateTime.now())
+                .httpStatus(HttpStatus.OK)
+                .message("Success getting article")
+                .data(data)
+                .build();
+    }
+
+    private Page<ArticleEntity> getArticleFromDB(ArticleRequestParam param, Pageable pageable){
+        log.info("Getting article from DB...");
+
+        List<ParamFilter> paramFilters = param.getFilters();
+        Page<ArticleEntity> data = null;
+
+        if(paramFilters.isEmpty())
+            data = articleRepository.findAll(pageable);
+        else
+            data = articleRepository.findAll(specificationCreator.getSpecification(paramFilters), pageable);
+
+        return data;
+    }
+    
     @Transactional
     public DataResponse<ArticleResponseDto> save(ArticleRequestDto articleDto){
         log.info("article: {}", articleDto);
@@ -55,6 +120,27 @@ public class ArticleService {
                 .httpStatus(HttpStatus.OK)
                 .message("Success saving Article")
                 .data(data)
+                .build();
+
+    }
+
+    @Transactional
+    public DataResponse<String> delete(Integer articleId){
+        log.info("deleting article with id: {}",articleId);
+        UserEntity currentUser = audit.getCurrentAuditor().orElseThrow(() -> new BadRequestException("Request not authenticated"));
+        log.info("current authenticated user: {}",currentUser.getUsername());
+
+        ArticleEntity article = articleRepository.findByIdAndUserId(articleId, currentUser.getId()).orElseThrow(() -> new BadRequestException("Article not Found"));
+
+        log.info("deleting...");
+        articleRepository.delete(article);
+        log.info("deleted");
+
+        return DataResponse.<String>builder()
+                .timestamp(LocalDateTime.now())
+                .httpStatus(HttpStatus.OK)
+                .message("Success deleting Article")
+                .data("Article with id: "+ articleId + " deleted.")
                 .build();
 
     }
