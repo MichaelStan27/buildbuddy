332e9a4c1e55eb04113916db18efd0d7b7e90e99
exaltation <michstan96@gmail.com>
2024-03-31 12:28:22 +0700

    update get save article - add post, and image - get now can use search keyword

--- src/main/java/com/buildbuddy/domain/article/dto/response/article/ArticleResponseDto.java
+++ src/main/java/com/buildbuddy/domain/article/dto/response/article/ArticleResponseDto.java
@@ -1,6 +1,7 @@
 package com.buildbuddy.domain.article.dto.response.article;
 
 import com.buildbuddy.domain.article.entity.ArticleEntity;
+import com.buildbuddy.domain.article.entity.ArticleModel;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Base64;
 
 @Data
 @Builder
@@ -16,28 +18,50 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ArticleResponseDto {
 
-    @JsonProperty(value = "articleId")
+    @JsonProperty
     private Integer articleId;
 
-    @JsonProperty(value = "post")
+    @JsonProperty
+    private String title;
+
+    @JsonProperty
     private String post;
 
-    @JsonProperty(value = "username")
+    @JsonProperty
+    private String image;
+
+    @JsonProperty
     private String username;
 
+    @JsonProperty
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonProperty(value = "createdTime")
     private LocalDateTime createdTime;
 
+    @JsonProperty
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonProperty(value = "lastUpdateTime")
     private LocalDateTime lastUpdateTime;
 
     public static ArticleResponseDto convertToDto(ArticleEntity entity){
+        byte[] image = entity.getImage();
         return ArticleResponseDto.builder()
                 .articleId(entity.getId())
+                .title(entity.getTitle())
                 .post(entity.getPost())
+                .image(image != null ? Base64.getEncoder().encodeToString(image) : null)
                 .username(entity.getUser().getUsername())
+                .createdTime(entity.getCreatedTime())
+                .lastUpdateTime(entity.getLastUpdateTime())
+                .build();
+    }
+
+    public static ArticleResponseDto convertToDto(ArticleModel entity){
+        byte[] image = entity.getImage();
+        return ArticleResponseDto.builder()
+                .articleId(entity.getArticleId())
+                .title(entity.getTitle())
+                .post(entity.getPost())
+                .image(image != null ? Base64.getEncoder().encodeToString(image) : null)
+                .username(entity.getUsername())
                 .createdTime(entity.getCreatedTime())
                 .lastUpdateTime(entity.getLastUpdateTime())
                 .build();
--- src/main/java/com/buildbuddy/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/buildbuddy/domain/article/repository/ArticleRepository.java
@@ -1,13 +1,26 @@
 package com.buildbuddy.domain.article.repository;
 
 import com.buildbuddy.domain.article.entity.ArticleEntity;
+import com.buildbuddy.domain.article.entity.ArticleModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Integer>, JpaSpecificationExecutor<ArticleEntity> {
 
         Optional<ArticleEntity> findByIdAndUserId(Integer id, Integer userId);
+
+        @Query(nativeQuery = true, value = "select a.article_id as articleId, u.username as username, a.title as title, " +
+                "a.post as post, a.image as image, a.created_time as createdTime, a.last_update_time as lastUpdateTime " +
+                "from article a join user u on a.user_id = u.user_id " +
+                "where u.username like (case when :search is null then u.username else :search end) " +
+                "or a.title like (case when :search is null then a.title else :search end) " +
+                "or a.post like (case when :search is null then a.post else :search end)")
+        Page<ArticleModel> getByCustomParam(@Param("search") String search, Pageable pageable);
 
 }
