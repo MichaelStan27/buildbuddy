78d560967235d97cf32445b17033fb6d1847b12a
exaltation <michstan96@gmail.com>
2024-04-02 20:03:47 +0700

    add article id to get article

--- src/main/java/com/buildbuddy/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/buildbuddy/domain/article/repository/ArticleRepository.java
@@ -18,9 +18,11 @@ public interface ArticleRepository extends JpaRepository<ArticleEntity, Integer>
         @Query(nativeQuery = true, value = "select a.article_id as articleId, u.username as username, a.title as title, " +
                 "a.post as post, a.image as image, a.created_time as createdTime, a.last_update_time as lastUpdateTime " +
                 "from article a join user u on a.user_id = u.user_id " +
-                "where u.username like (case when :search is null then u.username else :search end) " +
+                "where a.article_id = (case when :articleId is null then a.article_id else :articleId end)" +
+                "and (u.username like (case when :search is null then u.username else :search end) " +
                 "or a.title like (case when :search is null then a.title else :search end) " +
-                "or a.post like (case when :search is null then a.post else :search end)")
-        Page<ArticleModel> getByCustomParam(@Param("search") String search, Pageable pageable);
+                "or a.post like (case when :search is null then a.post else :search end) " +
+                ")")
+        Page<ArticleModel> getByCustomParam(@Param("articleId") Integer articleId, @Param("search") String search, Pageable pageable);
 
 }
